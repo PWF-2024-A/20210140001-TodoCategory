27be4772157d013c10a9b4f2e78d739e42e83013
Beatrix Devanti Agsi <beatrixdevanti21@gmail.com>
2024-05-31 20:18:14 +0700

    api : auth, read, search data

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+    Route::middleware('auth::sanctum')->group(function(){
+
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    });
+
